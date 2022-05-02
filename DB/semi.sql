2322ec98f3d1f015ee9703c85badf9ddbaa1b296
yea-rim <55575488+yea-rim@users.noreply.github.com>
2022-05-03 00:30:19 +0900

    Update semi.sql

--- DB/semi.sql
+++ DB/semi.sql
@@ -1,10 +1,10 @@
 -- 커뮤니티(홍보게시판)
 create table community(
 community_no number primary key,
-project_no number references project(project_no) on delete cascade not null,
-number_no number references member(member_no) on delete cascade not null,
+project_no references project(project_no) on delete cascade not null,
+number_no references member(member_no) on delete cascade not null,
 community_title varchar2(100) not null,
-community_content varchar2(3000) not null,
+community_content varchar2(4000) not null,
 community_time date default sysdate not null,
 community_readcount number default 0 not null,
 community_replycount number default 0 not null
@@ -14,8 +14,8 @@ create sequence community_seq;
 -- 커뮤니티댓글
 create table community_reply(
 reply_no number primary key,
-review_no number references community(community_no) on delete cascade,
-number_no number references member(member_no) on delete set null,
+community_no references community(community_no) on delete cascade,
+member_no references member(member_no) on delete set null,
 reply_time date default sysdate not null,
 reply_content varchar2(600) not null
 );
@@ -23,8 +23,8 @@ create sequence community_reply_seq;
 
 -- 커뮤니티 사진
 create table community_photo(
-community_no number references community(community_no) on delete cascade not null,
-attach_no number references attach(attach_no) on delete cascade not null,
+community_no references community(community_no) on delete cascade not null,
+attach_no references attach(attach_no) on delete cascade not null,
 primary key(community_no, attach_no)
 );
 
