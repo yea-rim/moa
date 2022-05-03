0bb70a8c50a2fe204aed3d24eda1f7d7b6777b50
Gang-Eun <tjfrkd7415@naver.com>
2022-05-03 20:15:50 +0900

    로그인 / 세션 구현

--- moa/src/main/java/moa/beans/MemberDao.java
+++ moa/src/main/java/moa/beans/MemberDao.java
@@ -1,5 +1,43 @@
 package moa.beans;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class MemberDao {
 
+	public MemberDto selectOne(String memberEmail) throws Exception {
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select * from member where member_email = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ps.setString(1, memberEmail);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		MemberDto memberDto;
+		
+		if(rs.next()) {
+			memberDto = new MemberDto();
+			
+			memberDto.setMemberNo(rs.getInt("member_no"));
+			memberDto.setMemberEmail(rs.getString("member_email"));
+			memberDto.setMemberPw(rs.getString("member_pw"));
+			memberDto.setMemberNick(rs.getString("member_nick"));
+			memberDto.setMemberPhone(rs.getString("member_phone"));
+			memberDto.setMemberJoinDate(rs.getDate("member_join_date"));
+			memberDto.setMemberPost(rs.getString("member_post"));
+			memberDto.setMemberBasicAddress(rs.getString("member_basic_address"));
+			memberDto.setMemberDetailAddress(rs.getString("member_detail_address"));
+			memberDto.setMemberRoute(rs.getString("member_route"));
+		} else {
+			memberDto = null; 
+		}
+		
+		con.close();
+		
+		return memberDto; 
+		
+	}
 }
---
+++ moa/src/main/java/moa/servlet/member/MemberLoginServlet.java
@@ -0,0 +1,57 @@
+package moa.servlet.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import moa.beans.MemberDao;
+import moa.beans.MemberDto;
+
+@WebServlet(urlPatterns = "/member/login.do")
+public class MemberLoginServlet extends HttpServlet{
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		try {
+			
+			// 준비
+			req.setCharacterEncoding("utf-8");
+			String memberEmail = req.getParameter("memberEmail");
+			String memberPw = req.getParameter("memberPw");
+			
+			
+			// 처리s
+			MemberDao memberDao = new MemberDao();
+			MemberDto memberDto = memberDao.selectOne(memberEmail);
+			
+			
+			// memberDto가 존재하면서 비밀번호 일치 여부 검사 
+			boolean isLogin = memberDto != null && memberDto.getMemberPw().equals(memberPw);
+			
+			System.out.println(isLogin);
+			
+			
+			// 출력 
+			if(isLogin) { // 로그인 성공 
+				resp.sendRedirect(req.getContextPath()); // 메인 페이지로 이동
+				
+				// 세션에 login 추가
+				req.getSession().setAttribute("login", memberDto.getMemberNo());
+				
+			} else { // 로그인 실패 
+				resp.sendRedirect(req.getContextPath()+"/member/login.jsp?error");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+		
+	}
+
+}
